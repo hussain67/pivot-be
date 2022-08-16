a7ecad72c2c63e5727d831eeb31201744dc8d2d6
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-08-16 16:57:27 +0100

    node V added in package.json

--- src/app.js
+++ src/app.js
@@ -31,7 +31,9 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }));
-
+app.get("/", (req, res) => {
+  res.send("Pivot api");
+});
 app.use("/api/v1", home);
 app.use("/public", express.static("public"));
 app.use("/api/v1/auth", authRouter);
