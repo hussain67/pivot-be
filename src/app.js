7d8ade7b80295f2542aa15b1e1f56234d62d3df9
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-08-18 02:17:13 +0100

    cors refactored'

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
 });
-app.use(cors({ origin: "https://pivot-fe-demo.herokuapp.com/" }));
+app.use(cors());
 //app.options("https://pivot-a.netlify.app", cors());
 require("dotenv").config();
 app.enable("trust proxy", 1);
